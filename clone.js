2a8067b37358da38d3cb1fcac4308f6080be8d15
Abdul Jabbar <abdulabase@gmail.com>
2020-09-07 05:46:45 +0100

    Update clone.js

--- clone.js
+++ clone.js
@@ -1,34 +1,56 @@
-const shallowCloneObject = obj => ({...obj})
-const shallowCloneArray = arr => [...arr]
+export const shallowCloneObject = obj => ({...obj})
+export const shallowCloneArray = arr => [...arr]
 
-
-function deepCloneArray(arr){
-	const clonedArray = {...arr}
-
-	arr.forEach(index => {
-		switch (typeof index) {
-			case 'object':
-				return Array.isArray(index) ? deepCloneArray(index) : deepCloneObject(index)
+export function deepClonePrimitive(value) {
+	if (value === null) {
+		return null
+	} else {
+		switch (typeof value) {
+			case 'number':
+				return Number(value)
+			case 'string':
+				return String(value)
+			case 'boolean':
+				return Boolean(value)
+			case 'undefined':
+				return undefined
 			default:
 				break
 		}
-	})
+	}
+}
 
+
+export function deepCloneArray(arr){
+	const clonedArray = []
+
+	arr.forEach((item, index) => {
+		if (typeof item !== 'object' || item === null) {
+			clonedArray[index] = deepClonePrimitive(item)
+		} else if (typeof item === 'object') {
+			clonedArray[index] = Array.isArray(item) ? deepCloneArray(item) : deepCloneObject(item)
+		} else {
+			console.log('Deep Clone FAILED')
+			return console.log('EXCEPTION HERE!!!', typeof index, index)
+		}
+	})
 	return clonedArray
 }
 
-function deepCloneObject(obj){
-	const clonedObject = {...obj}
+export function deepCloneObject(obj){
+	const clonedObject = {}
 	const keys = Object.keys(obj)
 
 	keys.forEach(key => {
-		switch (typeof obj[key]) {
-			case 'object':
-				return Array.isArray(obj[key]) ? deepCloneArray(clonedObject[key]) : deepCloneObject(clonedObject[key])
-			default:
-				break
+		if (typeof obj[key] !== 'object' || obj[key] === null) {
+			clonedObject[key] = deepClonePrimitive(obj[key])
+		} else if (typeof obj[key] === 'object') {
+			clonedObject[key] = Array.isArray(obj[key]) ? deepCloneArray(obj[key]) : deepCloneObject(obj[key])
+		} else {
+			console.log('Deep Clone FAILED')
+			return console.log('EXCEPTION!!!', typeof obj[key], obj[key])
 		}
 	})
-
 	return clonedObject
 }
+
